d83fd0a47eba1c48f1e8665525411c3ed08cc43c
Gary Anderson <ganderson@seattleacademy.org>
2017-10-09 13:25:08 -0700

    Made changes for first class socket.io demo

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+function turnCube(face,amount){
+	console.log("face",face,"amount",amount);
+}
+
 io.on('connection', function(socket){
+  socket.on('turn cube', function(msg){
+  	turnCube("U","-1");
+  	console.log("turn cube",msg)
+    io.emit('turn cube', msg);
+  });
+
   socket.on('chat message', function(msg){
+  	console.log("received message",msg)
     io.emit('chat message', msg);
   });
 });
